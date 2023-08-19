39670532291dd3f0e2aac29ba3c231cbb9d0d2c6
martinsdesenvolvedor <107735914+martinsdesenvolvedor@users.noreply.github.com>
2023-08-19 15:20:45 -0300

    adicionado campo 'local_evento' no banco de dados, adicionado funções alteração e exclusão de dados

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(verbose_name='Descrição', blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    local_evento = models.CharField(verbose_name='Local do Evento', max_length=100, default='local')
     data_criacao = models.DateTimeField(verbose_name='Data de Criação', auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -17,4 +18,7 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y as %H:%MHs')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
 
